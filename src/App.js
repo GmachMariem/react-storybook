a8a1221639818eb1185c619ff5a1167f48c18b43
GmachMariem <mariemgmech@outlook.fr>
2022-03-24 15:21:22 +0100

    use callback

--- src/App.js
+++ src/App.js
@@ -3,37 +3,39 @@ import './App.css';
 import Autocomplete from "react-autocomplete";
 import "react-autocomplete-input/dist/bundle.css";
 import { Children, useEffect, useState } from 'react';
-import { useSearch ,useDebounce} from "./Hooks";
+import { useSearch, useDebounce, useSearchForm } from "./Hooks";
 import Input from './components/Input/index'
 
 function App() {
-  const[value,setValue]=useState('');
+  const{searchValue,onSearchChange}=useSearchForm()
 
-  const { articles } = useSearch(useDebounce(value,500));
+  const { articles } = useSearch(useDebounce(searchValue, 500));
 
   return (
     <>
-    
       <Autocomplete
         getItemValue={(item) => item.label}
         renderInput={Input}
-        inputProps={{placeholder:'Input a search term'}}
+        inputProps={{ placeholder: "Input a search term" }}
         items={articles}
-        renderMenu={(Children,value,style)=>(
-         <div style={{...style}} className='input-suggestions'>{Children}
-         <a href={`/search?query=${value}` } className="search-link">
-           See all results
-         </a>
-         </div>
-        )}
+        renderMenu={(Children, value, style) => {
+          return articles && articles.length ?
+          <div style={{ ...style }} className="input-suggestions">
+            {Children}
+            <a href={`/search?query=${value}`} className="search-link">
+              See all results
+            </a>
+          </div>
+          :<></>
+        }}
         renderItem={(item, isHighlighted) => (
           <div style={{ background: isHighlighted ? "lightgray" : "white" }}>
             {item.label}
           </div>
         )}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onSelect={(value) => setValue(value)}
+        value={searchValue}
+        onChange={onSearchChange}
+     
       />
     </>
   );
--- src/Hooks.js
+++ src/Hooks.js
@@ -1,4 +1,4 @@
-import { useEffect, useState ,useRef} from "react";
+import { useEffect, useState ,useRef,useCallback} from "react";
 import axios from "axios";
 
 export const useSearch = (query) => {
@@ -64,4 +64,17 @@ export const useDebounce=(value,delay)=>{
         }
     },[value,delay]);
     return debouncedValue
+}
+
+
+export const useSearchForm=()=>{
+const[searchValue,setSearchValue]=useState('');
+const onSearchChange=
+    useCallback((e)=>setSearchValue(e.target.value),[]);
+
+return {
+  searchValue,
+  onSearchChange,
+};
+
 }
